f3321bf915225518ce07983fa52b085648e70e38
pavellsan8 <pavelryan146@gmail.com>
2025-02-06 06:31:38 +0700

    Update versel.json 11, Adding logger in homepage views

--- pavel_sites/portofolio/views.py
+++ pavel_sites/portofolio/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+import logging
+logger = logging.getLogger(__name__)
 
 def homepage(request):
-    return render(request, 'homepage.html')
+    logger.error("Homepage view was called") 
+    try:
+        return render(request, 'homepage.html')
+    except Exception as e:
+        logger.error(f"Error rendering homepage: {str(e)}")
 
 def porto_details(request):
     return render(request, 'portfolio-details.html')
